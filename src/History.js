d1c8dbbf4559693c948cb8c4092bb3efe42743b4
Shaun Donnelly <shaun.donnelly@skyscanner.net>
2017-05-24 09:08:01 +0100

    I just can't help myself

--- src/History.js
+++ src/History.js
@@ -5,15 +5,15 @@ export default () => (
   <history>
     <h1>History</h1>
     <p>
-      In 2006 I graduated from the University of Sussex with a degree in Psychology BSc. (no, I don't have CS degree).
+      In 2006 I graduated from the University of Sussex with a degree in Psychology BSc. (no, I don't have a CS degree).
       During university a fascination with computing, and specifically web development, grew into a real passion.
       Since getting my first job in software in 2006 I have worked in engineering ever since.
     </p>
     <p>
       From 2011 until March 2017 I worked at <a href="https://www.ensighten.com/" target="_blank">Ensighten</a> in
-      various role from Senior Engineer to Technical Director.
+      various roles from Senior Engineer to Technical Director.
       During my time at Ensighten I was lucky enough to be able to build a great team and work under some great managers.
-      The team and I built very high-performance marketing/advertising/analytics SaaS platforms and accompanying frontends.
+      The team and I built very high-performance marketing/advertising/analytics SaaS platforms and accompanying front-ends.
     </p>
   </history>
 )
