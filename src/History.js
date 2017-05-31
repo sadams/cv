9c16b02ca8e817723060903690b03408fb4466e7
Sam Adams <sam.adams@ensighten.com>
2017-05-31 14:34:39 +0100

    more history

--- src/History.js
+++ src/History.js
@@ -9,11 +9,28 @@ export default () => (
       During university a fascination with computing, and specifically web development, grew into a real passion.
       Since getting my first job in software in 2006 I have worked in engineering ever since.
     </p>
+    <p className="quietly">For the next 5 years I worked at a couple of marketing agencies
+      on lead generation platforms
+      (<a href="https://www.linkedin.com/company/edr-agency" target="_blank">EDR</a> / <a href="https://en.wikipedia.org/wiki/Progressive_Digital_Media" target="_blank">TMN Group</a>) and
+      at <a href="https://www.premierit.com/" target="_blank">Premier IT</a> on
+      a bespoke CMS platform.
+    </p>
     <p>
-      From 2011 until March 2017 I worked at <a href="https://www.ensighten.com/" target="_blank">Ensighten</a> in
-      various role from Senior Engineer to Technical Director.
-      During my time at Ensighten I was lucky enough to be able to build a great team and work under some great managers.
-      The team and I built very high-performance marketing/advertising/analytics SaaS platforms and accompanying frontends.
+      Most recently (2011 through 2017) I worked
+      at <a href="https://www.ensighten.com/" target="_blank">Ensighten</a>.
+      I started as a Senior Engineer and finished as a Technical Director.
+      During my time at Ensighten I was lucky enough to be able to build a great team and work under some excellent managers.
+    </p>
+    <p>
+      Among other things, my team and I were responsible for building:
+    </p>
+    <ul>
+      <li>A <a href="https://en.wikipedia.org/wiki/Tag_management_system" target="_blank">Tag Management</a> platform.</li>
+      <li>A high-performance <a href="https://www.ensighten.com/products/pulse/" target="_blank">data-collection/analytics platform</a>.</li>
+      <li>An Attribution system (which interestingly <a href="http://searchengineland.com/google-attribution-search-marketers-need-know-275751" target="_blank">google have recently launched</a>).</li>
+    </ul>
+    <p>
+
     </p>
   </history>
 )
